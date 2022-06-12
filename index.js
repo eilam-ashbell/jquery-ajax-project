a561642a5fe73d85653135f3255efbb5a9f6c60a
eilam-ashbell <eashbell@gmail.com>
2022-06-13 01:15:24 +0300

    switches and modal

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const DEV_MODE = true;
 $.ajax({
   type: "get",
   url: "https://api.coingecko.com/api/v3/coins/list",
-//   data: "data",
-//   dataType: "json",
+  //   data: "data",
+  //   dataType: "json",
   success: (res) => insertCardsToDom(res),
 });
 
@@ -42,7 +42,7 @@ function insertCardsToDom(arr) {
   let cards = "";
   if (DEV_MODE) {
     //DEV_MODE is for quick loading - only 6 coins cards
-    const shortArr = arr.slice(0, 6);
+    const shortArr = arr.slice(0, 10);
     shortArr.forEach((coin) => {
       cards += createCoinCard(coin);
     });
@@ -60,12 +60,12 @@ function insertCardsToDom(arr) {
 // coin object example:
 // {id: '01coin', symbol: 'zoc', name: '01coin'}
 function createCoinCard(coinObj) {
-  return `<div class="card col-lg-4 col-md-12">
+  return `<div class="card col-lg-4 col-12">
     <div class="card-body" data-id="${coinObj.id}" data-name="${coinObj.name}" data-symbol="${coinObj.symbol}">
         <div class="d-flex flex-row justify-content-between">
             <h5 class="card-title">${coinObj.symbol}</h5>
             <div class="form-check form-switch">
-                <input class="form-check-input" type="checkbox" role="switch" id="select-${coinObj.symbol}">
+                <input class="form-check-input" type="checkbox" role="switch" id="select-${coinObj.symbol}" data-coin-symbol="${coinObj.symbol}" onclick="setToReport('${coinObj.symbol}')">
             </div>
         </div>
         <p class="card-text">${coinObj.name}</p>
@@ -109,4 +109,76 @@ function createMoreInfo(obj) {
           )}</td>
       </tbody>
   </table>`;
+}
+
+// REPORTS
+
+let coinsToReport = [];
+const reportModal = new bootstrap.Modal("#reportModal");
+
+function setToReport(coin) {
+  if (event.target.checked) {
+    if (coinsToReport.length >= 5) {
+      $("#saveModal").attr("data-coin-symbol", `${coin}`);
+      getModalData(coin);
+      $("#cancelModal").attr(
+        "data-coin-list",
+        `${JSON.stringify(coinsToReport)}`
+      );
+      reportModal.show();
+      $(event.target).prop("checked", false);
+    } else {
+      coinsToReport.push(coin);
+      $(`#select-${coin}`).prop("checked", true);
+      console.log(coinsToReport);
+    }
+  } else {
+    removeCoinFromReport(coin);
+  }
+}
+
+// set the active coins to the modal display
+function getModalData(coinName) {
+  $(".modal-table").html("");
+  $("#coinSpan").html(`"${coinName}"`);
+  coinsToReport.forEach((coin) => {
+    $(".modal-table")
+      .append(`<tr><td>${coin}</td><td><div class="form-check form-switch">
+        <input class="form-check-input" type="checkbox" role="switch" checked onclick="setToReport('${coin}')">
+        </div></td></tr>`);
+  });
+}
+
+// remove a coin from the report list
+function removeCoinFromReport(coin) {
+  coinsToReport = coinsToReport.filter((element) => element !== coin);
+  $(`#select-${coin}`).prop("checked", false);
+  console.log(coinsToReport);
+}
+
+function saveModalSettings(event) {
+  if (coinsToReport.length < 5) {
+    coinsToReport.push(event.target.dataset.coinSymbol);
+    $(`#select-${event.target.dataset.coinSymbol}`).prop("checked", true);
+    console.log(coinsToReport);
+  }
+  reportModal.hide();
+}
+
+function cancelModal(event) {
+  list = JSON.parse(event.target.dataset.coinList);
+  list.forEach((element) => {
+    $(`#select-${element}`).prop("checked", true);
+  });
+  coinsToReport = list;
+}
+
+// Navigation
+
+function changeTab(event) {
+  $(".nav-link").removeClass("active");
+  $(event.target).addClass("active");
+  $(".tab").hide();
+  console.log(event.target.dataset.tab);
+  $(`#${event.target.dataset.tab}`).show();
 }

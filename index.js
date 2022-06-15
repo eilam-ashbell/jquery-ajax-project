db7edbcf415390c60039e36f7658f96760655130
eilam-ashbell <eashbell@gmail.com>
2022-06-16 00:07:47 +0300

    search and filter

--- index.js
+++ index.js
@@ -63,18 +63,18 @@ function createCoinCard(coinObj) {
 
 // --- HANDLE NAVIGATION ---
 function changeTab(event) {
-    if ($(event.target).hasClass("nav-link")) {
+  if ($(event.target).hasClass("nav-link")) {
     $(".nav-link").removeClass("active");
     $(event.target).addClass("active");
     $(".tab").hide();
     $(`#${event.target.dataset.tab}`).show();
     if (event.target.dataset.tab === "live") {
-        initData()
+      initData();
     } else {
-        stopLiveReport()
-    }
+      stopLiveReport();
     }
   }
+}
 
 // --- HANDLE MORE INFO ---
 
@@ -203,125 +203,158 @@ function cancelModal(event) {
   coinsToReport = list;
 }
 
-
 // --- CHART ---
 
-    const canvas = document.getElementById("report");
-    let reportChart; //                                 will get the Chart object
-    const LABELS_NUMBER = 50; //                         define how much labels will be in the chart
-    const labels = new Array(LABELS_NUMBER); //         Array of all chart labels
-    let updateReportInterval; //                        will get the id of the chart update interval
-  
-    function initData() {
-      const dataset = []; //    Array of dataset objects
-      //                        datasetObject = {  label: coinName,
-      //                        yAxisID: coinName,
-      //                        data: data from API [],
-      //                        borderColor: string,
-      //                        backgroundColor: string}
-      fetch(
-        `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${coinsToReport}&tsyms=USD`
-      )
-        .then((res) => res.json())
-        .then((resJ) => {
-          coinsPrice = Object.entries(resJ);
-          // resJ example: [["HALF",{USD: 13659.5}],["ALGOHALF",{USD: 10666.5}],["BCHHALF",{USD: 6387}]]
-          coinsPrice.forEach((element, index) => {
-            dataset.push({
-              label: `${element[0]}`,
-              yAxisID: `A`,
-              data: [element[1].USD],
-              borderColor: getColor(index),
-              backgroundColor: getColor(index),
-            });
-          });
-          initLabels();
-          createChart(dataset, labels);
+const canvas = document.getElementById("report");
+let reportChart; //                                 will get the Chart object
+const LABELS_NUMBER = 50; //                         define how much labels will be in the chart
+const labels = new Array(LABELS_NUMBER); //         Array of all chart labels
+let updateReportInterval; //                        will get the id of the chart update interval
+
+function initData() {
+  const dataset = []; //    Array of dataset objects
+  //                        datasetObject = {  label: coinName,
+  //                        yAxisID: coinName,
+  //                        data: data from API [],
+  //                        borderColor: string,
+  //                        backgroundColor: string}
+  fetch(
+    `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${coinsToReport}&tsyms=USD`
+  )
+    .then((res) => res.json())
+    .then((resJ) => {
+      coinsPrice = Object.entries(resJ);
+      // resJ example: [["HALF",{USD: 13659.5}],["ALGOHALF",{USD: 10666.5}],["BCHHALF",{USD: 6387}]]
+      coinsPrice.forEach((element, index) => {
+        dataset.push({
+          label: `${element[0]}`,
+          yAxisID: `A`,
+          data: [element[1].USD],
+          borderColor: getColor(index),
+          backgroundColor: getColor(index),
         });
-    }
-  
-    function initLabels() {
-      const currentTime = new Date().getTime();
-      labels[0] = new Date(currentTime).toLocaleTimeString("en-US", {
-        hour12: false,
       });
-      for (let i = 1; i <= LABELS_NUMBER; i++) {
-        labels[i] = new Date(currentTime + 2000 * i).toLocaleTimeString("en-US", {
-          hour12: false,
+      initLabels();
+      createChart(dataset, labels);
+    });
+}
+
+function initLabels() {
+  const currentTime = new Date().getTime();
+  labels[0] = new Date(currentTime).toLocaleTimeString("en-US", {
+    hour12: false,
+  });
+  for (let i = 1; i <= LABELS_NUMBER; i++) {
+    labels[i] = new Date(currentTime + 2000 * i).toLocaleTimeString("en-US", {
+      hour12: false,
+    });
+  }
+}
+
+function createChart(dataset, labels) {
+  reportChart = new Chart(canvas, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: dataset,
+    },
+    options: {
+      animation: false,
+    },
+  });
+  startInterval(reportChart);
+}
+
+function startInterval(reportChart) {
+  updateReportInterval = setInterval(() => {
+    fetch(
+      `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${coinsToReport}&tsyms=USD`
+    )
+      .then((res) => res.json())
+      .then((resJ) => {
+        coinsPrice = Object.entries(resJ);
+        // resJ example: [["HALF",{USD: 13659.5}],["ALGOHALF",{USD: 10666.5}],["BCHHALF",{USD: 6387}]]
+        coinsPrice.forEach((element, i) => {
+          if (reportChart.data.datasets[i].data.length < LABELS_NUMBER) {
+            reportChart.data.datasets[i].data.push(element[1].USD);
+          } else {
+            reportChart.data.datasets[i].data.push(element[1].USD);
+            reportChart.data.datasets[i].data.shift();
+          }
         });
-      }
-    }
-  
-    function createChart(dataset, labels) {
-      reportChart = new Chart(canvas, {
-        type: "line",
-        data: {
-          labels: labels,
-          datasets: dataset,
-        },
-        options: {
-          animation: false,
-        },
+        if (reportChart.data.datasets[0].data.length >= LABELS_NUMBER) {
+          labels.push(
+            new Date(new Date().getTime() + 4000).toLocaleTimeString("en-US", {
+              hour12: false,
+            })
+          );
+          labels.shift();
+        }
+
+        reportChart.update();
       });
-      startInterval(reportChart);
+  }, 2000);
+}
+
+function stopLiveReport() {
+  clearInterval(updateReportInterval);
+  reportChart.destroy();
+}
+
+function getColor(index) {
+  switch (index) {
+    case 0:
+      return "#046E8F";
+      break;
+    case 1:
+      return "#70929F";
+      break;
+    case 2:
+      return "#38AECC";
+      break;
+    case 3:
+      return "#0090C1";
+      break;
+    case 4:
+      return "#183446";
+      break;
+  }
+}
+
+// --- SEARCH & FILTER---
+
+// this is the search we was asked to do
+// shows only if searching the exact term
+function handleSearch(term) {
+  if (term.length == 0) {
+    $(".card").show();
+  } else {
+    $(".card").hide();
+    $(`.card-body[data-symbol|="${term}"]`).parent().show();
+  }
+}
+
+// I think this is more user friendly search so i used this one..
+// shows all the coins that have the search term some where
+function newSearch () {
+    const term = $("#searchBox").val()
+    if (term.length == 0) {
+        $(".card").show();
+    } else {
+        $(".card").hide()
+        $(".card").has(`h5:contains(${term})`).show();
     }
-  
-    function startInterval(reportChart) {
-      updateReportInterval = setInterval(() => {
-        fetch(
-          `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${coinsToReport}&tsyms=USD`
-        )
-          .then((res) => res.json())
-          .then((resJ) => {
-            coinsPrice = Object.entries(resJ);
-            // resJ example: [["HALF",{USD: 13659.5}],["ALGOHALF",{USD: 10666.5}],["BCHHALF",{USD: 6387}]]
-            coinsPrice.forEach((element, i) => {
-              if (reportChart.data.datasets[i].data.length < LABELS_NUMBER) {
-                reportChart.data.datasets[i].data.push(element[1].USD);
-              } else {
-                reportChart.data.datasets[i].data.push(element[1].USD);
-                reportChart.data.datasets[i].data.shift();
-              }
-            });
-            if (reportChart.data.datasets[0].data.length >= LABELS_NUMBER) {
-              labels.push(
-                new Date(new Date().getTime() + 4000).toLocaleTimeString(
-                  "en-US",
-                  {
-                    hour12: false,
-                  }
-                )
-              );
-              labels.shift();
-            }
-  
-            reportChart.update();
-          });
-      }, 2000);
+}
+
+function toggleActive() {
+    if ($("#showActive").hasClass("active")) {
+        $(".card").show()
+        $("#showActive").text("Selected").css("padding", "6px 12px")
+        newSearch()
+    } else {
+        $("#showActive").text("All").css("padding", "6px 35px")
+        $(".card").has(`input:checked`).show()
+        $(".card").has(`input:not(:checked)`).hide()
     }
-  
-    function stopLiveReport() {
-      clearInterval(updateReportInterval);
-      reportChart.destroy();
-    }
-  
-    function getColor(index) {
-      switch (index) {
-        case 0:
-          return "#046E8F";
-          break;
-        case 1:
-          return "#70929F";
-          break;
-        case 2:
-          return "#38AECC";
-          break;
-        case 3:
-          return "#0090C1";
-          break;
-        case 4:
-          return "#183446";
-          break;
-      }
-    }
-  
+    $("#showActive").toggleClass("active")
+}
